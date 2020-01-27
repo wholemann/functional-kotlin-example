7207da4b8e1c27f0a57755f9c4bcc3524807326a
wholemann <wholeman.dev@gmail.com>
2020-01-27 12:28:36 +0900

    Add chapter 02

---
+++ 01/src/main/kotlin/com/wholeman/first/Property.kt
@@ -0,0 +1,12 @@
+package com.wholeman.first
+
+open class Cake(val flavour: String)
+
+class VanillaCake(flavour: String) : Cake(flavour) {
+    // ...
+}
+//class Cake(val flavour: String)
+
+fun main(args: Array<String>) {
+}
+
